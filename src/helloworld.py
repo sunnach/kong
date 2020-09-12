dbb71c7f742ba94a55cbc6b9c43c7d9d5ecbd77c
Sunny Achar <acharsunil.k@gmail.com>
2020-09-12 13:42:51 +0200

    examples

---
+++ src/helloworld.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+
+
+
+
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
+
+
+
+def greet(name):
+    print ('Hello', name)
+
+greet("sreeni")
